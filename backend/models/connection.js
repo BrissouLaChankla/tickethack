c4b0c38d35956f7d551162f74af527dbee866152
Brice Eliasse <brice.eliasse@gmail.com>
2023-01-25 10:11:11 +0100

    added env connection string

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:8oNRmK6eS41r7Aj2@cluster0.2dzubed.mongodb.net/tickethack';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
